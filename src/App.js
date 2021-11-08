d78162f6e06015b2ae3a2f1b09f767d1428e5dee
Viju <vsbrth.apache@gmail.com>
2021-11-08 16:47:36 +0530

    Remove some packages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import List from './components/List';
 import {Virtuoso} from 'react-virtuoso';
 import withListLoading from './components/withListLoading';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const ListLoading = withListLoading(List);
